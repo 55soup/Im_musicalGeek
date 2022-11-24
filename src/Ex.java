4d95c799a19cf7719db7ef992f5afc55ebbbb935
Backseonmi <w2102@e-mirim.hs.kr>
2022-11-24 12:05:34 +0900

    repo commit test

--- src/Ex.java
+++ src/Ex.java
@@ -2,7 +2,7 @@ public class Ex {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("백선미");
-
+        System.out.println("아닠 바보맞음ㅋ");
 
     }
 }
