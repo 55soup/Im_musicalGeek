ce64d3e36edf490660e4b750c93ef74804e896a4
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-11-30 18:58:37 +0900

    달력 수정

--- src/getAPI.java
+++ src/getAPI.java
@@ -13,35 +13,48 @@ import org.w3c.dom.NodeList;
 
 
 public class getAPI {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        final String KEY = "c89c9b35a0354e8e9a6d47b43834b769";
-        String genre_code = "AAAB"; //뮤지컬 장르 코드
-        String URL = "https://www.kopis.or.kr/openApi/restful/pblprfr?service=d2ff79b5a9ef4f2a87d79a89e7e35de7&stdate=20220101&eddate=20221230&prfstate=02&cpage=1&rows=2000&shcate=AAAB";
-        String showID = "PF195242";
-        int page=1;
-        String detailURL = "http://www.kopis.or.kr/openApi/restful/pblprfr/" + showID + "?service=" + KEY;
-//        System.out.println(detailURL);
+    private static Element eElement;
 
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+//        System.out.println(detailURL);
+        parsing();
+    }
+
+    public static void parsing(){
         try{
+            final String KEY = "c89c9b35a0354e8e9a6d47b43834b769";
+            String genre_code = "AAAB"; //뮤지컬 장르 코드
+            String URL = "https://www.kopis.or.kr/openApi/restful/pblprfr?service=d2ff79b5a9ef4f2a87d79a89e7e35de7&stdate=20220101&eddate=20221230&prfstate=02&cpage=1&rows=2000&shcate=AAAB";
             DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
             Document doc = dBuilder.parse(URL);
 
             // 제일 첫번째 태그
             doc.getDocumentElement().normalize();
-
             // 파싱할 tag
             NodeList nList = doc.getElementsByTagName("db");
 
+            //nList.getLength();
             for(int i=0; i<nList.getLength(); i++){
                 Node nNode = nList.item(i);
-                Element eElement = (Element) nNode;
-                System.out.println("뮤지컬코드 : " + getTagValue("mt20id", eElement));
+                eElement = (Element) nNode;
+                String showID = getTagValue("mt20id", eElement);
+                System.out.println("뮤지컬코드 : " + getTagValue("mt20id", eElement)); //뮤지컬 코드
+                String detailURL = "http://www.kopis.or.kr/openApi/restful/pblprfr/" + showID + "?service=" + KEY;
+                System.out.println(detailURL);
+                String posterURL = getPosterURL(eElement);
+//                DocumentBuilderFactory dbFactoty2 = DocumentBuilderFactory.newInstance();
+//                DocumentBuilder dBuilder2 = dbFactoty.newDocumentBuilder();
+//                Document doc2 = dBuilder.parse(detailURL);
+//                doc2.getDocumentElement().normalize();
+//                NodeList nList2 = doc.getElementsByTagName("db");
+//                Node nNode2 = nList.item(i);
+//                Element eElement2 = (Element) nNode;
+
             }
         }catch (Exception e){
             e.printStackTrace();
         }
-
     }
 
     // tag값의 정보를 가져오는 함수
@@ -53,5 +66,10 @@ public class getAPI {
         result = nlList.item(0).getTextContent();
 
         return result;
+    }
+
+    public static String getPosterURL(Element eElement){
+        String showID2 = getTagValue("poster", eElement);
+        return showID2;
     }
 }
